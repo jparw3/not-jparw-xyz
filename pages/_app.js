4015f4d97171516fe6ffba9b620c9832158c3a15
Jack <jparw33@gmail.com>
2022-03-30 20:34:23 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <div className="bg-[#1F1F1F] pt-8 min-h-screen debug-screens">
+  return <div className="bg-[#1F1F1F] pt-8 min-h-screen">
     <Component {...pageProps} />
   </div>
 }
